1de2724280f9e81dce270da3b86ecf1c1f369b41
Aaron Shiao <122342078+ashiaotw@users.noreply.github.com>
2023-12-20 22:44:09 -0500

    Update mem.v

--- mem.v
+++ mem.v
@@ -6,25 +6,36 @@ module mem(
   MemReq,
   RdWrBar,
   clk,
-  DataOut
+  data_out
 );
 
-parameter words = 4096 
-parameter DataWidth = 32;
-parameter AddrWidth = 24;
+  parameter words = 4096 
+  parameter DataWidth = 32;
+  parameter AddrWidth = 24;
 
-input [DataWidth - 1 : 0 ] DataIn;
-input [AddrWidth - 1 : 0] AddrWidth;
-input MemReq;
-input RdWrBar;
-input clk;
+  input [DataWidth - 1 : 0 ] DataIn;
+  input [AddrWidth - 1 : 0] AddrWidth;
+  input MemReq;
+  input RdWrBar;
+  input clk;
 
-output [DataWidth - 1 : 0] DataOut;
+  output [DataWidth - 1 : 0] data_out;
 
-reg [DataWidth - 1 : 0] Mem_Data [0 : words - 1];
+  reg [DataWidth - 1 : 0] Mem_Data [0 : words - 1];
 
-wire [DataWidth - 1 : 0] Data;
+  wire [DataWidth - 1 : 0] data;
 
+  // Read 
+  assign data = (MemReq && RdWrBar)? mem_data [address]:32'hz;  //high impedance or don't care
+  assign #accesstime data_out = data;  //delay
+  
+  // Write
+  always @(posedge clk) begin
+    if (MemReq && ~RdWrBar) begin
+      mem_data [Address] <= DataIn;
+    end
+  end
+endmodule
 
 
 
