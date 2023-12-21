bf61fb55b47618a48088ee09b7a1e8fe24eb43b5
Aaron Shiao <122342078+ashiaotw@users.noreply.github.com>
2023-12-20 22:25:26 -0500

    Update mem.v

--- mem.v
+++ mem.v
@@ -1,1 +1,31 @@
-ˋtimescale 1ns / 1ps
+`timescale 1ns / 1ps
+
+module mem(
+  DataIn,
+  Address,
+  MemReq,
+  RdWrBar,
+  clk,
+  DataOut
+);
+
+parameter words = 4096 
+parameter DataWidth = 32;
+parameter AddrWidth = 24;
+
+input [DataWidth - 1 : 0 ] DataIn;
+input [AddrWidth - 1 : 0] AddrWidth;
+input MemReq;
+input RdWrBar;
+input clk;
+
+output [DataWidth - 1 : 0] DataOut;
+
+reg [DataWidth - 1 : 0] Mem_Data [0 : words - 1];
+
+wire [DataWidth - 1 : 0] Data;
+
+
+
+
+
